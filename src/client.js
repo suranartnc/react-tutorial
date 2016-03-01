32f4f9bb11e4b65c3f01c66d37296e1d59b5386f
Suranart Niamcome <suranartnc@gmail.com>
2016-03-01 17:19:59 +0700

    Create a simple component without JSX

--- src/client.js
+++ src/client.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import App from './App'
+/* Create a simple component without JSX
+	<div>
+		React Tutorial
+	</div> 
+*/
+const App = function() {
+	return React.createElement('div', null, 'React Tutorial');
+}
 
 ReactDOM.render(<App />, document.getElementById('app'))
