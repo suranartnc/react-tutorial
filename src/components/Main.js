ab543579a9635ac6c54e49190042a8262edba75f
Suranart Niamcome <suranartnc@gmail.com>
2016-03-01 19:13:45 +0700

    set initial state

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,13 +7,15 @@ export default class Main extends Component {
 	
 	constructor(props) {
 		super(props);
-		this.articles = mockData;
+		this.state = {
+			articles: mockData
+		}
 	}
 
 	render() {
 		return (
 			<div className="col-md-8">
-				{ this.articles.map(function(article, index) {
+				{ this.state.articles.map(function(article, index) {
 					return (
 						<Article key={ article.id } article={ article } />
 					);
