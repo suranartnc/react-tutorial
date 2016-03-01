67dd0481bac1a8e4f8ee693396cd9f0674327f8b
SuranartN <suranartnc@gmail.com>
2016-03-02 00:19:17 +0700

    promote App to a class component to make react hot module replacement works

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Header from './Header';
 import Main from './Main';
 import Sidebar from './Sidebar';
 import Footer from './Footer';
 
-export default function() {
-	return (
-		<div className="container">
-			<Header />
-			<div className="row">
-				<Main />
-				<Sidebar />
+export default class App extends Component {
+	
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<Header />
+				<div className="row">
+					<Main />
+					<Sidebar />
+				</div>
+				<Footer />
 			</div>
-			<Footer />
-		</div>
-	);
+		);
+	}
 }
