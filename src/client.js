1dcb2f90b03565dc69b6a567352cd39e58d6a006
Suranart Niamcome <suranartnc@gmail.com>
2016-03-01 17:25:04 +0700

    move App component into a separated file

--- src/client.js
+++ src/client.js
@@ -1,18 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-/* Create a simple component without JSX
-	<div>
-		React Tutorial
-	</div> 
-*/
-const App = function() {
-	// return React.createElement('div', null, 'React Tutorial');
-	return (
-		<div>
-			React Tutorial
-		</div>
-	);
-}
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('app'))
---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = function() {
+	return (
+		<div>
+			React Tutorial
+		</div>
+	);
+}
+
+export default App;
