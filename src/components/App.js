0c17bfb6d44aeffc9886b36ade3f212818d5b16e
Suranart Niamcome <suranartnc@gmail.com>
2016-03-01 18:35:55 +0700

    composition

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import Header from './Header';
+import Main from './Main';
+import Sidebar from './Sidebar';
+import Footer from './Footer';
+
 export default function() {
 	return (
 		<div>
-			React Tutorial
+			<Header />
+			<div>
+				<Main />
+				<Sidebar />
+			</div>
+			<Footer />
 		</div>
 	);
 }
