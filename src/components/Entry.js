f2a4c0bf7cf71ca4ed16db6c79ec7d1346764faf
SuranartN <suranartnc@gmail.com>
2016-03-02 22:46:50 +0700

    fetch article data using the parameter given by react router

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
 
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
 export default class Entry extends Component {
 	
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			article: {}
+		}
+
+		this.getArticleById(this.props.params.id);
+	}
+
+	getArticleById(id) {
+		fetch(`http://localhost:3004/articles/${id}`)
+		    .then(function(response) {
+		        if (response.status >= 400) {
+		            throw new Error("Bad response from server");
+		        }
+		        return response.json();
+		    })
+		    .then((data) => {
+		        this.setState({
+		        	article: data
+		        });
+		    });
 	}
 
 	render() {
 		return (
 			<div>
 				<div className="col-md-8">
-					{ this.props.params.id }
+					<article>
+						<h1>{ this.state.article.title }</h1>
+						<div dangerouslySetInnerHTML={{ __html: this.state.article.body }} />
+					</article>
 				</div>
 				<div className="col-md-4">
 					Sidebar
